3c9d84cea439a289409da5a1638a2b5fc75a3012
Steve French <steve@eventbrite.com>
2011-09-13 10:03:23 -0700

    made the sadness epic

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<html>
+<title>I came by to say hi, but you weren't here....:(</title>
+<body bgcolor="black" text="white">
 <center>
 <img src="sad.jpg">
+<br>
+<h1>I came by to say hi, but you weren't here</h1>
 </center>
+</body>
+</html>
